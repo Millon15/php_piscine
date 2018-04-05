fd70b8f4e0b91e1bdb6775ba8b6a158bfc6ba909
vbrazas <appleitunesstore11@gmail.com>
2018-04-05 15:49:30 +0300

    d02 started

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -1,9 +1,11 @@
 <?php
 	function ft_is_sort($array)
 	{
-		$ar_buf = $array;
-		sort($ar_buf);
-		if ($ar_buf === $array)
+		$ar_buf0 = $array;
+		$ar_buf1 = $array;
+		sort($ar_buf0);
+		rsort($ar_buf1);
+		if ($ar_buf0 === $array || $ar_buf1 === $array)
 			return TRUE;
 		else
 			return FALSE;
--- d02/ex01/one_more_time.php
+++ d02/ex01/one_more_time.php
@@ -1,12 +1,44 @@
 #!/usr/bin/php
 <?php
 	if ($argc != 2) {
-		echo "Wrong Format\n";
 		exit (1);
 	}
 
-	if (preg_match("//", $argv[1], $matches) === 0) {
+	if (preg_match("/^[A-Za-z][a-z]+ (\d\d?) ([A-Za-z][a-z]+) (\d\d\d\d) (\d\d):(\d\d):(\d\d)$/", $argv[1], $matches) === 0) {
 		echo "Wrong Format\n";
 		exit (2);
 	}
+
+	$month = strtolower($matches[2]);
+	if ($month == "janvier")
+		$mo = 1;
+	else if ($month == "février")
+		$mo = 2;
+	else if ($month == "mars")
+		$mo = 3;
+	else if ($month == "avril")
+		$mo = 4;
+	else if ($month == "mai")
+		$mo = 5;
+	else if ($month == "juin")
+		$mo = 6;
+	else if ($month == "juillet")
+		$mo = 7;
+	else if ($month == "aout")
+		$mo = 8;
+	else if ($month == "septembre")
+		$mo = 9;
+	else if ($month == "octobre")
+		$mo = 10;
+	else if ($month == "novembre")
+		$mo = 11;
+	else if ($month == "décembre")
+		$mo = 12;
+
+	date_default_timezone_set("Europe/Paris");
+	$res = mktime($matches[4], $matches[5], $matches[6], $mo, $matches[1], $matches[3]);
+	if ($res != 0)
+		printf("%u\n", $res);
+	else
+		echo "Wrong Format\n";
 ?>
