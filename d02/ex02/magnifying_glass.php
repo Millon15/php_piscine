22d8ba6aa55b8d85014ded804b5d7751850da3b2
vbrazas <appleitunesstore11@gmail.com>
2018-04-05 18:44:31 +0300

    ex02 done

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -3,13 +3,20 @@
 	if ($argc != 2) {
 		exit (1);
 	}
+	$file = file_get_contents($argv[1]);
 	if (($file = file_get_contents($argv[1]))) {
 		preg_match_all("/(<a.*title=)(\".*\")(.*>.*<\/a>)/", $file, $mat);
-		print_r($mat);
 		$i = 0;
 		foreach ($mat[0] as $val) {
 			$upp = $mat[1][$i].strtoupper($mat[2][$i]).$mat[3][$i];
 			$file = str_replace($mat[1][$i].$mat[2][$i].$mat[3][$i], $upp, $file);
+			$i++;
+		}
+		preg_match_all("/(<a[^<]*>)([^<]*<)/", $file, $mat);
+		$i = 0;
+		foreach ($mat[0] as $val) {
+			$upp = $mat[1][$i].strtoupper($mat[2][$i]);
+			$file = str_replace($mat[1][$i].$mat[2][$i], $upp, $file);
 			$i++;
 		}
 		echo $file;
