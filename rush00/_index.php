2b68fd8f3e5ff7644bcf30a9250032e7c790d218
vbrazas <appleitunesstore11@gmail.com>
2018-04-08 11:50:24 +0300

    added rush00 folder

---
+++ rush00/_index.php
@@ -0,0 +1,89 @@
+<?php
+	//Подключаемся к mysql
+	$conn = mysqli_init();
+	if (!$conn) {
+		die('mysqli_init failed');
+	}
+	if (!mysqli_options($conn, MYSQLI_INIT_COMMAND, "SET AUTOCOMMIT = 0")) {
+		die('MYSQLI_INIT_COMMAND failed');
+	}
+	if (!mysqli_real_connect($conn,"localhost","root","4837gqHA","ft_minishop")){
+		die("mysqli_real_connect failed: " . mysqli_connect_error());
+	}
+	//Наполняем массив категорий в зависимости от того, что записано в таблицу категорий
+	$categories = array();
+	if ($result = mysqli_query($conn, 'SELECT * FROM categories')) {
+		while($tmp = mysqli_fetch_assoc($result)) {
+			$categories[] = $tmp;
+		}
+		mysqli_free_result($result);
+	}
+	//print_r($categories);
+	//Наполняем массив продуктов и привязывем к категориям
+	$products = array();
+	$cat = isset($_REQUEST['cat']) ? (int) $_REQUEST['cat'] : 0;
+	$sql = 'SELECT p.* FROM products AS p ';
+	if ($cat) {
+		$sql .= ' INNER JOIN categories_products AS cp ON cp.id_product=p.id AND cp.id_category=' . $cat;
+	}
+	if ($result = mysqli_query($conn, $sql)) {
+		while($tmp = mysqli_fetch_assoc($result)) {
+			$products[] = $tmp;
+		}
+		mysqli_free_result($result);
+	}
+
+	//Выкачиваем юзеров
+	$users = array();
+	if ($result = mysqli_query($conn, 'SELECT * FROM users')) {
+		while($tmp = mysqli_fetch_assoc($result)) {
+			$users[] = $tmp;
+		}
+		mysqli_free_result($result);
+	}
+	mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="description" content="Best Farm Products ever!">
+	<link rel="stylesheet" href="style.css">
+	<title>Farmer's Bazaar</title>
+</head>
+<body>
+	<!-- Если админ - добавиться-->
+	<header>
+		<nav class="navbar" role="navigation">
+			<div class="logo">
+				<a href="_index.php"><p>Farmer's Bazaar</p></a>
+			</div>
+			<div class="header-categories">
+				<?php
+					foreach($categories as $category) {
+						echo ' <a href="?cat=' . $category['id'] . '" class="list-group-item">' . $category['title'] . '</a>';
+				}
+				?>
+			</div>
+		</nav>
+	</header>
+	<div class="content">
+		<div class="products-row">
+				<?php foreach($products as $product) {?>
+					<div class="product-card">
+						<div class="product-thumbnail">
+							<img src="<?php echo $product['img'];?>" alt="">
+							<div class="caption">
+								<div class="product-price"><h2>$<?php echo $product['price'];?></h2></div>
+								<div class="product-title"><h1><?php echo $product['title'];?></h1></div>
+								<div class="product-intro"><h4><?php echo $product['intro'];?></h4></div>
+								<div class="button"><button class="buy">BUY</button></div>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+		</div>
+	</div>
+	<footer>
+	</footer>
+</body>
+</html>
---
+++ rush00/install.php
@@ -0,0 +1,101 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "root"; //Пароль mysql, который ты задавал при установке МАМПА
+	$dbname = "ft_minishop";
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password);
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	// Удаляем старую БД
+	$sql = "DROP DATABASE IF EXISTS $dbname";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error killing db: " . mysqli_error($conn));
+	}
+	// Создаем БД
+	$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error creating database: " . mysqli_error($conn));
+	}
+	mysqli_close($conn);
+	
+	//Подключаемся к БД
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	// Создаем таблицу категории
+	$sql = "CREATE TABLE IF NOT EXISTS categories (
+			id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+			title VARCHAR(255) NOT NULL
+			)";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error creating categories: " . mysqli_error($conn));
+	}
+
+	// Наполняем таблицу категорий
+	$sql = "INSERT INTO categories (id, title)
+			VALUES (1, 'Fruits'), (2, 'Greens'), (3, 'Meat')";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error filling categories: " . mysqli_error($conn));
+	}
+
+	// Создаем таблицу продуктов
+	$sql = "CREATE TABLE IF NOT EXISTS products (
+			id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+			title VARCHAR(255) NOT NULL,
+			img VARCHAR(255) DEFAULT NULL,
+			category VARCHAR(255)DEFAULT NULL,
+			intro text NOT NULL,
+			price DECIMAL(10,0) NOT NULL
+			)";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error creating products: " . mysqli_error($conn));
+	}
+	// Наполняем таблицу продуктов
+	$sql = "INSERT INTO products (id, title, img, intro, price, category)
+			VALUES (1, 'Apples', 'http://faynofruit.com.ua/wp-content/uploads/2015/02/01-apple-mutsu.png', 'Apples are cool', '12', 'Fruits'),
+			(2, 'Onions', 'http://spicexpert.ru/files/Image/luk-rep.jpg', 'Onions are awful', '15', 'Greens'),
+			(3, 'Deer', 'http://arvi.by/wp-content/uploads/2017/12/%D0%B31-1-300x300.jpg', 'Deers lives matter', '50', 'Meat')";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error filling products: " . mysqli_error($conn));
+	}
+	// Создаем таблицу категорий продуктов для связи и заполняем ее в формате:
+	// (id категории, id товара)
+	$sql = "CREATE TABLE IF NOT EXISTS categories_products (
+			id_category INT(11) NOT NULL, 
+			id_product INT(11) NOT NULL,
+			PRIMARY KEY (id_category, id_product)
+			)";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error creating categories_products: " . mysqli_error($conn));
+	}
+	$sql = "INSERT INTO categories_products (id_category, id_product)
+			VALUES (1, 1), (2, 2), (3, 3)";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error filling categories: " . mysqli_error($conn));
+	}
+
+	// Создаем таблицу юзеров и добавляем админа
+	$sql = "CREATE TABLE IF NOT EXISTS users (
+			id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+			username VARCHAR(255) NOT NULL,
+			password VARCHAR(256) NOT NULL,
+			isadmin BOOLEAN NOT NULL
+			)";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error creating users: " . mysqli_error($conn));
+	}
+	$sql = "INSERT INTO users (id, username, password, isadmin)
+			VALUES (1, 'admin', 'admin', true)";
+	if (!mysqli_query($conn, $sql)) {
+		die("Error filling users: " . mysqli_error($conn));
+	}
+
+	//Вывожу для дебаггинга, убрать на продукте
+	echo "Installation complited";
+	mysqli_close($conn);
+?>
