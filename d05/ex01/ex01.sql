c7bb11dd4a883c4faee3ae707936267c9f60d03d
vbrazas <appleitunesstore11@gmail.com>
2018-04-09 18:02:23 +0300

    1st commit\)

---
+++ d05/ex01/ex01.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `ft_table` (`id` INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT, `login` VARCHAR(8) NOT NULL DEFAULT 'toto', `group` ENUM('staff', 'student', 'other') NOT NULL, `creation_date` DATE NOT NULL);
