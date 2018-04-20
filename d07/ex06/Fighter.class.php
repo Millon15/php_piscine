a2dec7061abd84650b303afbb66b8648406fb05f
vbrazas <appleitunesstore11@gmail.com>
2018-04-20 20:26:44 +0300

    d07 finally done

---
+++ d07/ex06/Fighter.class.php
@@ -0,0 +1,9 @@
+<?php
+	class Fighter {
+		public $fighterType;
+
+		public function __construct($fighterName) {
+			$this->fighterType = $fighterName;
+		}
+	}
+?>
---
+++ d07/ex06/UnholyFactory.class.php
@@ -0,0 +1,38 @@
+<?php
+	class UnholyFactory {
+		private $fighters = Array();
+
+		public function absorb($newFighter) {
+			if ($newFighter instanceof Fighter) {
+				$newFighterName = $newFighter->fighterType;
+
+				if (in_array($newFighter, $this->fighters))
+					print("(Factory already absorbed a fighter of type " . $newFighterName . ")" . PHP_EOL);
+				else {
+					print("(Factory absorbed a fighter of type " . $newFighterName . ")" . PHP_EOL);
+					$this->fighters["$newFighterName"] = $newFighter;
+				}
+			}
+			else {
+				print("(Factory can't absorb this, it's not a fighter)" . PHP_EOL);
+			}
+		}
+
+		public function fabricate($fighterToFabricate) {
+			$to_fabricate = FALSE;
+			foreach ($this->fighters as $key => $val) {
+				if ($fighterToFabricate == $key) {
+					$to_fabricate = TRUE;
+					break ;
+				}
+			}
+
+			if ($to_fabricate) {
+				print("(Factory fabricates a fighter of type " . $fighterToFabricate . ")" . PHP_EOL);
+				return (new $this->fighters["$fighterToFabricate"]);
+			}
+			else
+				print("(Factory hasn't absorbed any fighter of type " . $fighterToFabricate . ")" . PHP_EOL);
+		}
+	}
+?>
